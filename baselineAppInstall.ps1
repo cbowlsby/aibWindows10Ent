f0c34c31398147b944bb46ec2bcf58d20636fa0b
cbowlsby <80366140+cbowlsby@users.noreply.github.com>
2021-09-11 19:14:18 -0700

    Add files via upload

--- baselineAppInstall.ps1
+++ baselineAppInstall.ps1
@@ -1,22 +1,30 @@
 #create directory for installers
-New-Item -Path "c:\" -Name "installers" -ItemType "directory"
+#New-Item -Path "c:\" -Name "installers" -ItemType "directory"
 
+#install chocolatey
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+Start-Sleep -s 20
+
+choco install adobereader
+choco install 7zip.install
+choco install vlc
 
 #download installers
 ##1. 7-zip
-Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile "c:\installers\7z1900-x64.msi"
+#Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile "c:\installers\7z1900-x64.msi"
 
 ##2. adobe reader dc
-Invoke-WebRequest "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2100520060/AcroRdrDC2100520060_en_US.exe" -OutFile "c:\installers\AcroRdrDC2100520060_en_US.exe"
+#Invoke-WebRequest "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2100520060/AcroRdrDC2100520060_en_US.exe" -OutFile "c:\installers\AcroRdrDC2100520060_en_US.exe"
 
 ##3. PuTTY
-Invoke-WebRequest "https://the.earth.li/~sgtatham/putty/0.75/w64/putty-64bit-0.75-installer.msi" -OutFile "c:\installers\putty-64bit-0.75-installer.msi"
+#Invoke-WebRequest "https://the.earth.li/~sgtatham/putty/0.75/w64/putty-64bit-0.75-installer.msi" -OutFile "c:\installers\putty-64bit-0.75-installer.msi"
 
 #install apps silently
-Start-Process "c:\installers\7z1900-x64.msi" -ArgumentList "/S" -Wait
-Start-Process "c:\installers\AcroRdrDC2100520060_en_US.exe" -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
-Start-Process "c:\installers\putty-64bit-0.75-installer.msi" -ArgumentList "/qn" -Wait
+#Start-Process "c:\installers\7z1900-x64.msi" -ArgumentList "/S" -Wait
+#Start-Process "c:\installers\AcroRdrDC2100520060_en_US.exe" -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES" -Wait
+#Start-Process "c:\installers\putty-64bit-0.75-installer.msi" -ArgumentList "/qn" -Wait
 
 
 

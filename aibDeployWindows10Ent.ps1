28aa3647deafe000e633dc606c944926caefe508
cbowlsby <80366140+cbowlsby@users.noreply.github.com>
2021-09-12 15:45:08 -0700

    Add files via upload

--- aibDeployWindows10Ent.ps1
+++ aibDeployWindows10Ent.ps1
@@ -1,53 +1,41 @@
-# Step 1: Set up environment and variables
-
-# Step 1: Import module
-
+## Phase 1: Set up environment and variables
+# Import module
 Import-Module Az.Accounts
 
-# Step 2: get existing context
+# variables for existing context, destination image resource group, azure region, your subscription, image template name, 
+# and distribution properties object name (properties of the managed image on completion)
 $currentAzContext = Get-AzContext
-
-# destination image resource group
 $imageResourceGroup="AutomatedImagePipeline"
-
-# location (see possible locations in main docs)
 $location="westus2"
-
-# your subscription, this will get your current subscription
 $subscriptionID=$currentAzContext.Subscription.Id
-
-# image template name
-$imageTemplateName="10entM365Template"
-
-# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$imageTemplateName="10EntM365-Template"
 $runOutputName="sigOutput"
 
 # create resource group
 New-AzResourceGroup -Name $imageResourceGroup -Location $location
 
 
-## Step 2 : Permissions, create user identity and role for AIB
 
-# Create user identity
-# setup role def names, these need to be unique
+
+
+
+
+
+
+## Phase 2 : Permissions, create user identity and role for AIB
+# Create user identity, these values must be unique
 $timeInt=$(get-date -UFormat "%s")
 $imageRoleDefName="Azure Image Builder Image Def "+$timeInt
 $identityName="aibIdentity"+$timeInt
-
-# create identity
 New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
 
 $identityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
 $identityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
 
-# Assign permissions for identity to distribute images
-
+# Assign permissions for identity to distribute images, then download config and configure
 $aibRoleImageCreationUrl="https://raw.githubusercontent.com/cbowlsby/aib/main/aibRoleImageCreation.json"
 $aibRoleImageCreationPath = "aibRoleImageCreation.json"
-
-# download config
 Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
-
 ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
 ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
 ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
@@ -62,30 +50,29 @@ Start-Sleep -s 60
 # grant role definition to image builder service principal
 New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
 
-# Step 3 : Create the Shared Image Gallery
+
+
+
+
+
+
+
+## Phase 3 : Create the Shared Image Gallery
 $sigGalleryName= "w365SharedImageGallery"
-$imageDefName ="10EntM365Apps"
+$imageDefName ="10EntM365"
 
-# create gallery
+# create gallery and gallery definition
 New-AzGallery -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup  -Location $location
+New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType Windows -Publisher 'ChristopherBowlsby' -Offer 'Windows' -Sku '10EntM365'
 
-# create gallery definition
-New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType Windows -Publisher 'ChristopherBowlsby' -Offer 'Windows' -Sku '10EntM365Apps'
-
-
-# Configure the Image Template
-
-# Download template and configure
+# Download image template and configure
 $templateUrl="https://raw.githubusercontent.com/cbowlsby/aib/main/armTemplatew365.json"
 $templateFilePath = "armTemplatew365.json"
-
 Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
-
 ((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
-
 ((Get-Content -path $templateFilePath -Raw) -replace '<imageDefName>',$imageDefName) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
 ((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
